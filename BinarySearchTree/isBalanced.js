bbd8947330803b07a2105fb8766d656e11bb2f8c
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-21 10:27:46 +0300

    binarySearchTree

---
+++ BinarySearchTree/isBalanced.js
@@ -0,0 +1,52 @@
+var Height = function (node) {
+  if (node === null) {
+    return 0;
+  }
+  return Math.max(Height(node.left), Height(node.right)) + 1; // here I get the height of the left and right nodes and add 1 to add the parent node for the left and right nodes
+};
+
+var isBalanced = function (root) {
+  if (!root) {
+    return true; // if there is no nodes in the tree, it returns true
+  }
+
+  // here I make an inOrder traversal for the tree and use two stacks to push to and pop from them
+
+  let stack1 = [];
+  let stack2 = [];
+
+  stack1.push(root);  // I instantiate the tree by pushing the root node to stack1
+
+  while (stack1.length) {
+    // here I push the left and right nodes to the stack1 and then pop them from stack1 in the curr variable to push the curr to stack2
+
+    let curr = stack1.pop();
+
+    stack2.push(curr);
+
+    if (curr.left) 
+        stack1.push(curr.left);
+
+    if (curr.right) 
+        stack1.push(curr.right);
+  }
+
+  while (stack2.length) {
+    // here I pop from stack2 to check each node's height and then apply the balance rule to check if the tree is balanced or not
+    let curr = stack2.pop();
+
+    let rightHeight = 0;
+    let leftHeight = 0;
+
+    if (curr.left) 
+        leftHeight = Height(curr.left);
+
+    if (curr.right) 
+        rightHeight = Height(curr.right);
+
+    if (Math.abs(leftHeight - rightHeight) > 1) // here I apply the balance rule to compare between the leftHeight and the rightHeight
+        return false;
+  }
+  // if the leftHeight-rightHeight <= 1; it returns true
+  return true;
+};
---
+++ BinarySearchTree/symmetricTree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    if (!root) {
+        return true
+    }
+    return checkMirror(root.left, root.right)
+}
+var checkMirror = function (root1, root2) {
+    if (!root1 && !root2) {
+        return true
+    }
+    if ((!root1 || !root2) || (root1.val != root2.val)) {
+        return false;
+    }
+
+    return checkMirror(root1.left, root2.right) && checkMirror(root1.right, root2.left)
+
+}
