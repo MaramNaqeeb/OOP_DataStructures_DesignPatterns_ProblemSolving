8b5328af05852e86da5eb7c247747439f72503e4
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-21 10:31:40 +0300

    convertToBST

--- BinarySearchTree/covertToBST.js
+++ BinarySearchTree/covertToBST.js
@@ -19,24 +19,20 @@
  */
 
 var sortedListToBST = function (head) {
-
-    let arr = []
-    while (head) {
-        // here I create the array that I will convert to a BST
-        arr.push(head.val) // I instantiate the array by pushing the head value to the array
-        head = head.next   // here I push the nodes next to the head 
-
-    }
-    return convert(arr, 0, arr.length - 1)
+  let arr = [];
+  while (head) {
+    // here I create the array that I will convert to a BST
+    arr.push(head.val); // I instantiate the array by pushing the head value to the array
+    head = head.next; // here I push the nodes next to the head
+  }
+  return convert(arr, 0, arr.length - 1);
 };
 var convert = function (head, left, right) {
-    if (left > right)
-        return null
+  if (left > right) return null;
 
-    let mid = Math.floor((left + right) / 2)  // get the mid of the array
-    let root = new TreeNode(head[mid])        // creating the BST node
-    root.left = convert(head, left, mid - 1)
-    root.right = convert(head, mid + 1, right)
-    return root
-}
-
+  let mid = Math.floor((left + right) / 2); // get the mid of the array
+  let root = new TreeNode(head[mid]); // creating the BST node
+  root.left = convert(head, left, mid - 1);
+  root.right = convert(head, mid + 1, right);
+  return root;
+};
