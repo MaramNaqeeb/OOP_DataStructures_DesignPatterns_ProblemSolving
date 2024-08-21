893ee90973795ac0f1b4fb18fff54605bd85420a
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-21 19:51:40 +0300

    isBalancedTree

--- BinarySearchTree/isBalancedTree.js
+++ BinarySearchTree/isBalancedTree.js
@@ -3,49 +3,18 @@ var Height = function (node) {
     return 0;
   }
   // here I calculate the maximum height of the left and right nodes and add 1 for the current node to get the total height
-  return Math.max(Height(node.left), Height(node.right)) + 1; 
+  return Math.max(Height(node.left), Height(node.right)) + 1;
 };
 
 var isBalanced = function (root) {
   if (!root) {
-    return true; // if there is no nodes in the tree, it returns true
+    // if there is no nodes in the tree, it returns true
+    return true;
   }
-
-  // here I make an inOrder traversal for the tree (DFS) and use two stacks to push to and pop from them
-
-  let stack1 = []; // to reverse the tree by popping the tree from stack1 and pushing it to stack2
-  let stack2 = []; // to traverse the tree from the bottom as it has the reversed tree popped from stack1
-
-  stack1.push(root); // I initialize stack1 by pushing the root node to it
-
-  while (stack1.length) {
-    let curr = stack1.pop();
-
-    stack2.push(curr);
-
-    if (curr.left) 
-      stack1.push(curr.left);
-
-    if (curr.right) 
-      stack1.push(curr.right);
+  // here I apply the balance rule to compare between the leftHeight and the rightHeight
+  if (Math.abs(Height(root.left) - Height(root.right)) > 1) {
+    return false;
   }
-
-  while (stack2.length) {
-    // when stack2 becomes empty, I finish iterating through the tree
-    let curr = stack2.pop();
-
-    let rightHeight = 0;
-    let leftHeight = 0;
-
-    if (curr.left) 
-      leftHeight = Height(curr.left);
-
-    if (curr.right)
-       rightHeight = Height(curr.right);
-    // here I apply the balance rule to compare between the leftHeight and the rightHeight
-    if (Math.abs(leftHeight - rightHeight) > 1) 
-      return false;
-  }
-  // if the leftHeight-rightHeight <= 1; it returns true
-  return true;
+  // here I call the function recursively
+  return isBalanced(root.left) && isBalanced(root.right);
 };
