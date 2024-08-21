7e4c1d9873e48079d96653f081007e15015402e7
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-21 10:28:56 +0300

    sameTree

--- BinarySearchTree/sameTree.js
+++ BinarySearchTree/sameTree.js
@@ -11,13 +11,11 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    if(p=== null && q===null) return true;
-    if((p===null || q===null) || (p.val!=q.val)){
-        return false
-    }
-  
-      return  isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
-    
- 
+var isSameTree = function (p, q) {
+  if (p === null && q === null) return true;
+  if (p === null || q === null || p.val != q.val) {
+    return false;
+  }
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
