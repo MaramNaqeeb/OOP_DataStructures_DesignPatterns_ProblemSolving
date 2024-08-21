ef4da8b45c786825e28696be0a86f298a7f99a40
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-21 10:30:27 +0300

    symmetricTree

--- BinarySearchTree/symmetricTree.js
+++ BinarySearchTree/symmetricTree.js
@@ -11,19 +11,20 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
-    if (!root) {
-        return true
-    }
-    return checkMirror(root.left, root.right)
-}
+  if (!root) {
+    return true;
+  }
+  return checkMirror(root.left, root.right);
+};
 var checkMirror = function (root1, root2) {
-    if (!root1 && !root2) {
-        return true
-    }
-    if ((!root1 || !root2) || (root1.val != root2.val)) {
-        return false;
-    }
+  if (!root1 && !root2) {
+    return true;
+  }
+  if (!root1 || !root2 || root1.val != root2.val) {
+    return false;
+  }
 
-    return checkMirror(root1.left, root2.right) && checkMirror(root1.right, root2.left)
-
-}
+  return (
+    checkMirror(root1.left, root2.right) && checkMirror(root1.right, root2.left)
+  );
+};
