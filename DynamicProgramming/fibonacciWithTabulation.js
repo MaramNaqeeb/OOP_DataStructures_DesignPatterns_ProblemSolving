64b29c7d8feaadeb051e200b825c140134d4ec17
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-22 13:53:51 +0300

    fibonacciWithTabulation

---
+++ DynamicProgramming/fibonacciWithTabulation.js
@@ -0,0 +1,11 @@
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+  let arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
+console.log(fibonacci(3));
