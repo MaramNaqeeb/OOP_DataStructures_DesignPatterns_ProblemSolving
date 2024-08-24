1c8bfdc4e67c3e6d36cb519e43ce04e9ec6ede4c
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-24 15:46:52 +0300

    centerOfStarGraph

--- Graph/centerOfStarGraph.js
+++ Graph/centerOfStarGraph.js
@@ -5,7 +5,7 @@
 var findCenter = function (edges) {
     // here I compare the first element of the first array (first vertex) with the other arrays (vertices)
     if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
-        // I return the first element of the first array if the condition is met
+        // I return the first element of the first array if one of the above conditions is met
         return edges[0][0]
     }
         // I return the second element of the first array if the condition is not met
