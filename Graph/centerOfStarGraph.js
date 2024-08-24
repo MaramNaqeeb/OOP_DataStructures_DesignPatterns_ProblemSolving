8bb7ae94fdf7da734528c14e8e51fb3161df0426
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-24 15:53:11 +0300

    centerOfStarGraph

--- Graph/centerOfStarGraph.js
+++ Graph/centerOfStarGraph.js
@@ -8,7 +8,7 @@ var findCenter = function (edges) {
         // I return the first element of the first array if one of the above conditions is met
         return edges[0][0]
     }
-        // I return the second element of the first array if none of the above conditions is not met
+        // I return the second element of the first array if none of the above conditions is met
     return edges[0][1]
 }
 console.log(findCenter([[1,2],[2,3],[4,2]]))
