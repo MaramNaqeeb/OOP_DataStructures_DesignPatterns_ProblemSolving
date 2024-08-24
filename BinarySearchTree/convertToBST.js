89a1b8fcbb481ad3bc7f096961c2b4fbb3a490d8
Maram Naqeeb <maramnaqib1998@gmail.com>
2024-08-24 22:28:08 +0300

    convertToBST

--- BinarySearchTree/convertToBST.js
+++ BinarySearchTree/convertToBST.js
@@ -27,13 +27,13 @@ var sortedListToBST = function (head) {
     }
     return convert(arr, 0, arr.length - 1);
   };
-  var convert = function (head, left, right) {
+  var convert = function (arr, left, right) {
     if (left > right) return null;
   
     let mid = Math.floor((left + right) / 2); // I get the mid of the array
-    let root = new TreeNode(head[mid]); // here I create the BST root which is the mid element of the array
-    root.left = convert(head, left, mid - 1); // here I convert the left side of the array to be the left side of the tree
-    root.right = convert(head, mid + 1, right); // here I convert the right side of the array to be the right side of the tree
+    let root = new TreeNode(arr[mid]); // here I create the BST root which is the mid element of the array
+    root.left = convert(arr, left, mid - 1); // the left side of the array becomes the left side of the tree
+    root.right = convert(arr, mid + 1, right); // the right side of the array becomes the right side of the tree
     return root;
   };
   
